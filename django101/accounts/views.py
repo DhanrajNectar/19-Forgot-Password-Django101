c6f5523202359477f9e453a4a05378ac93825b7d
syawal2kalii <syawal20162016@gmail.com>
2020-04-30 14:25:06 +0800

    Rendering Data to Views

--- django101/accounts/views.py
+++ django101/accounts/views.py
@@ -1,21 +1,32 @@
-from cgi import log
-from logging import log
-
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import *
 
 # Create your views here.
 
 def home(request):
-
     return render(request, 'accounts/home.html')
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    total_customer = customers.count()
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    delivered = Order.objects.filter(status='Delivered').count()
+    pending = Order.objects.filter(status='Pending').count()
+
+    context = {
+        'customers': customers,
+        'orders': orders,
+        'total_orders': total_orders,
+        'total_customer': total_customer,
+        'delivered': delivered,
+        'pending': pending
+    }
+    return render(request, 'accounts/dashboard.html', context)

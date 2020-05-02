74e9222581900680fddfa136318f391fe250eab2
syawal2kalii <syawal20162016@gmail.com>
2020-05-02 15:11:13 +0800

    dynamic urls & url names

--- django101/accounts/urls.py
+++ django101/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard),
-    path('product/', views.products),
-    path('customer/<str:pk_test>/', views.customer),
+    path('', views.dashboard, name="dashboard"),
+    path('product/', views.products, name="products"),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
 ]

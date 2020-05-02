bbef486d49126e0257c967fdfbabde24123b036a
syawal2kalii <syawal20162016@gmail.com>
2020-05-02 14:47:39 +0800

    dynamic url

--- django101/accounts/views.py
+++ django101/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import *
 
 # Create your views here.
@@ -10,8 +11,14 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    customer = Customer.objects.get(id = pk_test)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
+
 
 def dashboard(request):
     customers = Customer.objects.all()

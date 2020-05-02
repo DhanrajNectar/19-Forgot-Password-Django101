b4cd244b14738b4da08060bfed0124fa5d2ffd3a
syawal2kalii <syawal20162016@gmail.com>
2020-05-03 06:06:00 +0800

    CRUD

--- django101/accounts/models.py
+++ django101/accounts/models.py
@@ -40,12 +40,10 @@ class Order(models.Model):
             ('Delivered', 'Delivered'),
             )
 
-    name = models.CharField(max_length=200, null=True)
-    price = models.FloatField(null=True)
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
 
     def __str__(self):
-        return str(self.id)
+        return str(self.product.name)
--- django101/accounts/views.py
+++ django101/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .forms import OrderForm
 from .models import *
 
 # Create your views here.
@@ -37,3 +39,39 @@ def dashboard(request):
         'pending': pending
     }
     return render(request, 'accounts/dashboard.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        # print('Printing POST:', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+def delete_order(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method=='POST':
+        order.delete()
+        return redirect('/')
+
+    context = {'item': order}
+    return render(request, 'accounts/delete.html', context)
+
+

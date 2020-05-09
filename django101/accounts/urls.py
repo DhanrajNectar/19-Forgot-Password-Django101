a74fa6ac7d2dcca19434574b553174fb720c470e
syawal2kalii <syawal20162016@gmail.com>
2020-05-09 15:21:21 +0800

    Forgot Password

--- django101/accounts/urls.py
+++ django101/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
@@ -12,5 +13,17 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('user/', views.userPage, name='user-page'),
-    path('account/', views.accountSettings, name='account')
+    path('account/', views.accountSettings, name='account'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'),
+         name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_complete')
+    
+
+
 ]
